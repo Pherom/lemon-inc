d099847f68d027b828b69665fa396e267bb15aec
Pherom <pheromem@gmail.com>
2023-05-08 21:06:55 +0300

    Added full lemon glass and the ability to squeeze lemons and make juice using the juicer

--- Assets/Code/Scripts/SetJuicerState.cs
+++ Assets/Code/Scripts/SetJuicerState.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class SetJuicerState : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject lemonJuicePrefab;
     private GameObject closed;
     private GameObject opened;
 
-    public void Start()
+    void Start()
     {
         closed = gameObject.transform.GetChild(0).gameObject;
         opened = gameObject.transform.GetChild(1).gameObject;
@@ -16,6 +19,24 @@ public class SetJuicerState : MonoBehaviour
 
     public void ToggleState()
     {
+        if (opened.activeSelf)
+        {
+            //Check if lemon in socket and drinking glass in socket
+            if (opened.GetComponent<XRSocketInteractor>().hasSelection)
+            {
+                if (!gameObject.GetComponent<XRSocketInteractor>().hasSelection || gameObject.GetComponent<XRSocketInteractor>().GetOldestInteractableSelected().transform.gameObject.CompareTag("Full Glass"))
+                {
+                    return;
+                }
+
+                GameObject drinkingGlass = gameObject.GetComponent<XRSocketInteractor>().GetOldestInteractableSelected().transform.gameObject;
+                GameObject lemonSlice = opened.GetComponent<XRSocketInteractor>().GetOldestInteractableSelected().transform.gameObject;
+                Instantiate(lemonJuicePrefab, drinkingGlass.transform.position, drinkingGlass.transform.rotation);
+                Destroy(drinkingGlass);
+                Destroy(lemonSlice);
+            }
+        }
+
         closed.SetActive(!closed.activeSelf);
         opened.SetActive(!opened.activeSelf);
     }
