605882e8a88746ab7ccdfdb9c538b9a380700d81
Pherom <pheromem@gmail.com>
2023-05-16 19:15:02 +0300

    Added walking animations to customers

--- Assets/Code/Scripts/Customers/CustomerNavMesh.cs
+++ Assets/Code/Scripts/Customers/CustomerNavMesh.cs
@@ -10,10 +10,14 @@ public class CustomerNavMesh : MonoBehaviour
     [SerializeField]
     private Vector3 orderContactPosition;
     private NavMeshAgent navMeshAgent;
+    private Animator animator;
+    private bool reachedOrderApproachPosition = false;
+    private bool reachedOrderContactPosition = false;
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
         navMeshAgent.destination = orderApproachPosition;
     }
 
@@ -21,7 +25,21 @@ public class CustomerNavMesh : MonoBehaviour
     {
         if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
         {
-            navMeshAgent.destination = orderContactPosition;
+            if (!reachedOrderApproachPosition)
+            {
+                reachedOrderApproachPosition = true;
+                navMeshAgent.destination = orderContactPosition;
+            }
+
+            else if (!reachedOrderContactPosition)
+            {
+                reachedOrderContactPosition = true;
+            }
+
+            else
+            {
+                animator.SetFloat("Speed_f", 0);
+            }
         }
     }
 }
